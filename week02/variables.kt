abcfe03467d1830f5fd9040f37a5c58c5ddcf676
jwalk333 <100548748+jwalk333@users.noreply.github.com>
2023-02-11 15:38:02 -0600

    Create variables.kt

---
+++ week02/variables.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val myAge = 30      
+    var averageAge = (myAge + 30)/2
+    val testNumber = 4
+    val evenOdd = testNumber%2
+    var answer = 0
+println(averageAge)
+println(evenOdd)
+println(answer++ +10 *10 *1000)
+}
+
