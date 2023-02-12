2ba5f5d21a876d944e83a6c9713f79bcd14d55a7
jwalk333 <100548748+jwalk333@users.noreply.github.com>
2023-02-12 10:58:24 -0600

    Create stringMani.kt

---
+++ week02/stringMani.kt
@@ -0,0 +1,7 @@
+fun main(){
+    var userCity = "Nashville"
+    println(userCity.uppercase())
+    println(userCity.lowercase())
+    println("The number of characters in your favorite city is: " + userCity.length)
+    println("The first character of your favorite city is: " + userCity[0])
+ }
