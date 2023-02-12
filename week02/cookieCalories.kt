2c4f1f82a0fd91b850c223a15c9e74f0f1315876
jwalk333 <100548748+jwalk333@users.noreply.github.com>
2023-02-12 10:05:13 -0600

    Create cookieCalories.kt

---
+++ week02/cookieCalories.kt
@@ -0,0 +1,7 @@
+fun main(){
+	val caloriePerCookie = 75
+  var cookies = 3
+  var caloriesEaten = caloriePerCookie * cookies
+  println("Number of cookies eaten: " + cookies)
+  println("Total calories consumed: " + caloriesEaten)
+}
