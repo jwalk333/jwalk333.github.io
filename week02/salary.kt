e322929eee83ce8ad57c44bc013b40e2cbe4d29e
jwalk333 <100548748+jwalk333@users.noreply.github.com>
2023-02-12 09:35:23 -0600

    Create salary.kt

---
+++ week02/salary.kt
@@ -0,0 +1,6 @@
+fun main(){
+    var name = "Jessica Walker"
+    var age = 30
+    var annualPay: Double = 100000.00
+    println("My name is " + name + ", my age is "+ age + " and I hope to earn $" + annualPay + " per year.")
+}
