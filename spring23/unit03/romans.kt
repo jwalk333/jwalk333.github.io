f56eb0079a561db0d6e9879b2538f0ddfc2b7e01
jwalk333 <100548748+jwalk333@users.noreply.github.com>
2023-02-23 19:49:43 -0600

    Update romans.kt

--- spring23/unit03/romans.kt
+++ spring23/unit03/romans.kt
@@ -1,15 +1,17 @@
-fun main() {
-    var number = 8
-    when(number){
-        1->println("I")
-        2->println("II")
-        3->println("III")
-        4->println("IV")
-        5->println("V")
-        6->println("VI")
-        7->println("VII")
-        8->println("VIII")
-        9->println("IX")
-        10->println("X")
-        else->println("error")}
-}
+fun main(args : Array<String>) {
+    print("Enter a number from 1-10: ")
+    var input = readLine()!!
+    var number: Int = input.toInt()
+        when(number)
+        {1->println("I")
+            2->println("II")
+            3->println("III")
+            4->println("IV")
+            5->println("V")
+            6->println("VI")
+            7->println("VII")
+            8->println("VIII")
+            9->println("IX")
+            10->println("X")
+            else->println("error")}
+    }
