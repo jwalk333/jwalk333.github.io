8ecc1456dcfa2bc5327148ca2c9a4d4e76f42e93
jwalk333 <100548748+jwalk333@users.noreply.github.com>
2023-02-23 20:20:41 -0600

    Create massWeight.kt

---
+++ spring23/unit03/massWeight.kt
@@ -0,0 +1,10 @@
+fun main(args : Array<String>) {
+    print("Enter your object's mass in kilograms: ")
+    var input1 = readLine()!!
+    var mass: Int = input1.toInt()
+    val weight = mass * 9.8
+    println("Your object weighs $weight Newtons.")
+    if(weight > 1000){println("Your object is too heavy.")}
+    else if(weight < 10){println("Your object is too light.")}
+    else{println("Your object's weight is optimal.")}
+    }
