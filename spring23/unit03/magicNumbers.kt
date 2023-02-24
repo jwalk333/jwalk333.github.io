aafc263d11c77193434607e83efd3a8a0a647280
jwalk333 <100548748+jwalk333@users.noreply.github.com>
2023-02-23 19:56:11 -0600

    Update magicNumbers.kt

--- spring23/unit03/magicNumbers.kt
+++ spring23/unit03/magicNumbers.kt
@@ -1,8 +1,15 @@
-fun main() {
-  var month = 2
-  var day = 15
-  var year = 80
-  val magicNumber = month * day 
-  if(year==magicNumber){println("The day is magic")}
+fun main(args : Array<String>) {
+    print("Enter a month in numeric form: ")
+    var input1 = readLine()!!
+    var month: Int = input1.toInt()
+    print("Now enter a day: ")
+    var input2 = readLine()!!
+    var day: Int = input2.toInt()
+    print("Finally, enter a 2-digit year: ")
+    var input3 = readLine()!!
+    var year: Int = input3.toInt()
+    val magicNumber = month * day
+    if(year==magicNumber){println("The day is magic")}
     else{println("The day is not magic")}
+
 }
