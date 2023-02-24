4085fbfff08c1fee44c2f77ebab416d47e80451d
jwalk333 <100548748+jwalk333@users.noreply.github.com>
2023-02-23 20:12:03 -0600

    Update stringMani.kt

--- spring23/week02/stringMani.kt
+++ spring23/week02/stringMani.kt
@@ -1,7 +1,8 @@
-fun main(){
-    var userCity = "Nashville"
-    println(userCity.uppercase())
-    println(userCity.lowercase())
-    println("The number of characters in your favorite city is: " + userCity.length)
-    println("The first character of your favorite city is: " + userCity[0])
- }
+fun main(args : Array<String>) {
+    print("Enter a city: ")
+    var input = readLine()!!
+    println(input.uppercase())
+    println(input.lowercase())
+    println("The number of characters in your favorite city is: " + input.length)
+    println("The first character of your favorite city is: " + input[0])
+}
