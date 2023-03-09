17e72afae99e56475571ed91c9f70cd58f2e358d
jwalk333 <100548748+jwalk333@users.noreply.github.com>
2023-03-09 11:21:20 -0600

    Create guessingGame.kt

---
+++ spring23/unit04/guessingGame.kt
@@ -0,0 +1,26 @@
+fun main() {
+    val min = 1
+    val max = 100
+    val randomNumber = (Math.random() * (max - min + 1)).toInt() + min
+    var userInput: Int? = null
+    while (userInput == null) {
+        print("Enter a number between 1 and 100: ")
+        val input = readLine()
+        try {
+            userInput = input?.toInt()
+            if (userInput != null && userInput < randomNumber) {
+                println("Too low. Guess again.")
+                userInput = null
+            }
+            else if(userInput != null && userInput > randomNumber){
+                println("Too high. Guess again")
+                userInput = null
+            }
+            else {
+                println("Correct!")
+            }
+        } catch (e: Exception) {
+            println("Invalid input. Please enter a number between 1 and 100.")
+        }
+    }
+}
